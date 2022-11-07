4ddf89f8b478093dc5e3fc2cf73653c8c71280cd
nurilloh2004 <nurillohpython@gmail.com>
2022-11-07 16:03:19 +0500

    stop

--- myprint/help_views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import get_object_or_404, redirect, render
-from .models import *
-from .forms import OrderServiceForm
-
-def handle_upload_file(f):
-    with open('media/forma/' + f.files , 'wb+') as destiantion:
-        for chunk in f.chunks():
-            destiantion.write(chunk)
